26e7c940199204bbdfc7bd9cd7a651a7028bc1af
fi3ework <fi3ework@gmail.com>
2019-12-27 01:58:00 +0800

    feat: successful fetch all docs

--- src/preset/index.ts
+++ src/preset/index.ts
@@ -1,3 +1,7 @@
+import { ensureDirSync, outputFile } from 'fs-extra'
+import { Base64 } from 'js-base64'
+import path from 'path'
+
 import Octokit, { ReposGetContentsResponse } from '@octokit/rest'
 
 import { ANTD_GITHUB } from './constant'
@@ -7,9 +11,19 @@ const octokit = new Octokit({
   auth: GITHUB_TOKEN,
 })
 
-const IGNORED_COMPONENTS: string[] = ['__tests__', '_util']
+const IGNORED_COMPONENTS: string[] = [
+  '__tests__',
+  '_util',
+  'locale',
+  'style',
+  'version',
+  'col',
+  'row',
+]
 
-async function getContents(tag: string) {
+type IShaMap = { [k: string]: { enSha: string; zhSha: string } }
+
+async function genShaMap(tag: string) {
   const tagContentRes = await octokit.repos.getContents({
     owner: ANTD_GITHUB.OWNER_NAME,
     repo: ANTD_GITHUB.REPO_NAME,
@@ -18,40 +32,56 @@ async function getContents(tag: string) {
   })
 
   const contentData: ReposGetContentsResponse = tagContentRes.data
-  if (!Array.isArray(contentData)) return
+  if (!Array.isArray(contentData)) return {}
 
   const componentPaths = contentData
     .filter(c => !IGNORED_COMPONENTS.includes(c.name))
     .map(c => c.name)
 
-  const shaMap: { [k: string]: { enSha: string; zhSha: string } } = {}
-  const ps = componentPaths.slice(0, 3).map(name => {
-    return octokit.repos
-      .getContents({
-        owner: ANTD_GITHUB.OWNER_NAME,
-        repo: ANTD_GITHUB.REPO_NAME,
-        path: `/components/${name}`,
-        ref: tag,
-      })
-      .then(res => {
-        if (Array.isArray(res.data)) {
-          shaMap[name] = {
-            enSha: res.data.find(file => file.name === ANTD_GITHUB.EN_MD_NAME)!.sha,
-            zhSha: res.data.find(file => file.name === ANTD_GITHUB.ZH_MD_NAME)!.sha,
-          }
-        }
-      })
+  const shaMap: IShaMap = {}
+  const promises = componentPaths.map(async name => {
+    const folderRes = await octokit.repos.getContents({
+      owner: ANTD_GITHUB.OWNER_NAME,
+      repo: ANTD_GITHUB.REPO_NAME,
+      path: `/components/${name}`,
+      ref: tag,
+    })
+    if (Array.isArray(folderRes.data)) {
+      shaMap[name] = {
+        enSha: folderRes.data.find(file => file.name === ANTD_GITHUB.EN_MD_NAME)!.sha,
+        zhSha: folderRes.data.find(file => file.name === ANTD_GITHUB.ZH_MD_NAME)!.sha,
+      }
+    }
   })
 
-  Promise.all(ps).then(() => {
-    console.log(shaMap)
+  await Promise.all(promises)
+  return shaMap
+}
+
+async function downloadFile(componentName: string, fileName: string, fileSha: string) {
+  const contentRes = await octokit.git.getBlob({
+    owner: ANTD_GITHUB.OWNER_NAME,
+    repo: ANTD_GITHUB.REPO_NAME,
+    file_sha: fileSha,
+  })
+  ensureDirSync(path.resolve(__dirname, `../doc/${componentName}`))
+  await outputFile(
+    path.resolve(__dirname, `../doc/${componentName}/${fileName}`),
+    Base64.decode(contentRes.data.content)
+  )
+  console.log(`✅ ${componentName}/${fileName} download succeed.`)
+}
+
+async function downloadByShaMap(shaMap: IShaMap) {
+  Object.entries(shaMap).forEach(async ([componentName, entity]) => {
+    downloadFile(componentName, ANTD_GITHUB.EN_MD_NAME, entity.enSha)
+    downloadFile(componentName, ANTD_GITHUB.ZH_MD_NAME, entity.zhSha)
   })
 }
 
-// async function get
-
 async function init() {
-  await getContents(ANTD_GITHUB.TARGET_TAG)
+  const shaMap = await genShaMap(ANTD_GITHUB.TARGET_TAG)
+  downloadByShaMap(shaMap)
 }
 
 init()
